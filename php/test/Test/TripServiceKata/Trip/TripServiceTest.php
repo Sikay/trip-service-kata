a2cc7504133e6e57a4e607d5f7ea4b04a096f0ec
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-05-16 15:26:14 +0200

    should_not_return_any_trips_when_users_are_not_friends

--- php/test/Test/TripServiceKata/Trip/TripServiceTest.php
+++ php/test/Test/TripServiceKata/Trip/TripServiceTest.php
@@ -4,6 +4,7 @@ namespace Test\TripServiceKata\Trip;
 
 use PHPUnit\Framework\TestCase;
 use TripServiceKata\Exception\UserNotLoggedInException;
+use TripServiceKata\Trip\Trip;
 use TripServiceKata\Trip\TripService;
 use TripServiceKata\User\User;
 
@@ -11,6 +12,8 @@ class TripServiceTest extends TestCase
 {
     private const GUEST = null;
     private const UNUSED_USER = null;
+    private const REGISTERED_USER = 'Francisco';
+    private const ANOTHER_USER = 'Pepe';
 
     private TesteableTripService $tripService;
 
@@ -26,6 +29,19 @@ class TripServiceTest extends TestCase
         $this->tripService->loggedInUser = self::GUEST;
 
         $this->tripService->getTripsByUser(new User(self::UNUSED_USER));
+    }
+
+    /** @test */
+    public function should_not_return_any_trips_when_users_are_not_friends() {
+
+        $friend = new User('Juan');
+        $friend->addFriend(new User(self::ANOTHER_USER));
+        $friend->addTrip(new Trip());
+
+        $this->tripService->loggedInUser = new User(self::REGISTERED_USER);;
+        $friendTrip = $this->tripService->getTripsByUser($friend);
+
+        self::assertTrue(sizeof($friendTrip) === 0);
     }
 
 }
